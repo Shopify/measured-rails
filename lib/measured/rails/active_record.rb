d6065d9121c9bd89c420e3f5c9b255411b3276f7
Kevin McPhillips <github@kevinmcphillips.ca>
2018-02-08 22:53:57 -0500

    Do not redefine a method that has already been defined.

--- lib/measured/rails/active_record.rb
+++ lib/measured/rails/active_record.rb
@@ -5,7 +5,6 @@ module Measured::Rails::ActiveRecord
     def measured(measured_class, *fields)
       options = fields.extract_options!
       options = {}.merge(options)
-      defined_unit_accessors = []
 
       measured_class = measured_class.constantize if measured_class.is_a?(String)
       unless measured_class.is_a?(Class) && measured_class.ancestors.include?(Measured::Measurable)
@@ -28,57 +27,60 @@ module Measured::Rails::ActiveRecord
         value_field_name = "#{ field }_value"
 
         # Reader to retrieve measured object
-        define_method(field) do
-          value = public_send(value_field_name)
-          unit = public_send(unit_field_name)
+        unless method_defined?(field)
+          define_method(field) do
+            value = public_send(value_field_name)
+            unit = public_send(unit_field_name)
 
-          return nil unless value && unit
+            return nil unless value && unit
 
-          instance = instance_variable_get("@measured_#{ field }") if instance_variable_defined?("@measured_#{ field }")
-          new_instance = begin
-            measured_class.new(value, unit)
-          rescue Measured::UnitError
-            nil
-          end
+            instance = instance_variable_get("@measured_#{ field }") if instance_variable_defined?("@measured_#{ field }")
+            new_instance = begin
+              measured_class.new(value, unit)
+            rescue Measured::UnitError
+              nil
+            end
 
-          if instance == new_instance
-            instance
-          else
-            instance_variable_set("@measured_#{ field }", new_instance)
+            if instance == new_instance
+              instance
+            else
+              instance_variable_set("@measured_#{ field }", new_instance)
+            end
           end
         end
 
         # Writer to assign measured object
-        define_method("#{ field }=") do |incoming|
-          if incoming.is_a?(measured_class)
-            instance_variable_set("@measured_#{ field }", incoming)
-            precision = self.column_for_attribute(value_field_name).precision
-            scale = self.column_for_attribute(value_field_name).scale
-            rounded_to_scale_value = incoming.value.round(scale)
+        unless method_defined?("#{ field }=")
+          define_method("#{ field }=") do |incoming|
+            if incoming.is_a?(measured_class)
+              instance_variable_set("@measured_#{ field }", incoming)
+              precision = self.column_for_attribute(value_field_name).precision
+              scale = self.column_for_attribute(value_field_name).scale
+              rounded_to_scale_value = incoming.value.round(scale)
 
-            max = self.class.measured_fields[field][:max_on_assignment]
-            if max && rounded_to_scale_value > max
-              rounded_to_scale_value = max
-            elsif rounded_to_scale_value.to_i.to_s.length > (precision - scale)
-              raise Measured::Rails::Error, "The value #{rounded_to_scale_value} being set for column '#{value_field_name}' has too many significant digits. Please ensure it has no more than #{precision - scale} significant digits."
+              max = self.class.measured_fields[field][:max_on_assignment]
+              if max && rounded_to_scale_value > max
+                rounded_to_scale_value = max
+              elsif rounded_to_scale_value.to_i.to_s.length > (precision - scale)
+                raise Measured::Rails::Error, "The value #{rounded_to_scale_value} being set for column '#{value_field_name}' has too many significant digits. Please ensure it has no more than #{precision - scale} significant digits."
+              end
+
+              public_send("#{ value_field_name }=", rounded_to_scale_value)
+              public_send("#{ unit_field_name }=", incoming.unit.name)
+            else
+              instance_variable_set("@measured_#{ field }", nil)
+              public_send("#{ value_field_name}=", nil)
+              public_send("#{ unit_field_name }=", nil)
             end
-
-            public_send("#{ value_field_name }=", rounded_to_scale_value)
-            public_send("#{ unit_field_name }=", incoming.unit.name)
-          else
-            instance_variable_set("@measured_#{ field }", nil)
-            public_send("#{ value_field_name}=", nil)
-            public_send("#{ unit_field_name }=", nil)
           end
         end
 
-        next if defined_unit_accessors.include?(unit_field_name)
-
         # Writer to override unit assignment
-        define_method("#{ unit_field_name }=") do |incoming|
-          defined_unit_accessors << unit_field_name
-          unit_name = measured_class.unit_system.unit_for(incoming).try!(:name)
-          write_attribute(unit_field_name, unit_name || incoming)
+        unless method_defined?("#{ unit_field_name }=")
+          define_method("#{ unit_field_name }=") do |incoming|
+            unit_name = measured_class.unit_system.unit_for(incoming).try!(:name)
+            write_attribute(unit_field_name, unit_name || incoming)
+          end
         end
       end
     end
