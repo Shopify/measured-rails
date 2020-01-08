7ef0a7bfe7b7179fd28d93bfd32f280fbad18363
Kevin McPhillips <github@kevinmcphillips.ca>
2020-01-08 17:31:50 -0500

    Bump version to 2.5.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+2.5.2
+-----
+
+* Fix some deprecations in tests and CI.
+
 2.5.1
 -----
 
--- lib/measured/rails/version.rb
+++ lib/measured/rails/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Measured
   module Rails
-    VERSION = "2.5.1"
+    VERSION = "2.5.2"
   end
 end
